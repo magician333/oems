55b1f90b7ee7bd95f1c1e7858b04c55bdf345c57
purplefire <magician33333@163.com>
2019-10-21 21:41:54 +0800

    add free-used function

--- manager/views.py
+++ manager/views.py
@@ -27,6 +27,8 @@ def return_tools(request):
 def returned(request, order_id):
     # 将是否归还设置为True
     order = RentOrder.objects.get(id=order_id)
+    used2free(order.tools.split(","))
+    print(order.tools.split(","))
     order.is_returned = True
     order.save()
     return render(request, "return_ok.html")
@@ -41,13 +43,34 @@ def free2used(wtList):
     for i in wtList:
         for j in All:
             if i in j.free_tools:
-                # print(j.free_tools.split(","))
-                # print(j.free_tools.split(",").index(i))
-                j.free_tools.split(",").pop(
-                    j.free_tools.split(",").index(i))
-                print(j.free_tools.split(","))
-        # j.used_tools = i+","+j.used_tools
-        # j.save()
+                f_tools = j.free_tools.split(",")
+                f_tools.remove(i)
+                j.free_tools = ",".join(f_tools)
+                # 复制free_tools,删除相应值并赋值回去
+                if j.used_tools == "":
+                    u_tools = []
+                else:
+                    u_tools = j.used_tools.split(",")
+                j.used_tools = ",".join(u_tools+i.split(" "))
+                # 检查used_tool是否为空，并赋值，将添加的设备放入used_tools
+                j.save()
+
+
+def used2free(wtList):
+    for i in wtList:
+        for j in All:
+            if i in j.used_tools:
+                u_tools = j.used_tools.split(",")
+                u_tools.remove(i)
+                j.used_tools = ",".join(u_tools)
+                # 复制used_tools,删除相应值并赋值回去
+                if j.free_tools == "":
+                    f_tools = []
+                else:
+                    f_tools = j.free_tools.split(",")
+                j.free_tools = ",".join(f_tools+i.split(" "))
+                # 检查free_tool是否为空，并赋值，将添加的设备放入free_tools
+                j.save()
 
 
 def order(request):
@@ -55,7 +78,7 @@ def order(request):
     ro = RentOrder()
     ro.name = request.POST["name"]
     ro.tools = ",".join(request.POST.getlist("tool"))  # 列表转字符串
-    # free2used(request.POST.getlist("tool"))
+    free2used(request.POST.getlist("tool"))
     ro.regard = request.POST["regard"]
     ro.phone = request.POST["phone"]
     ro.date = request.POST["date"]
